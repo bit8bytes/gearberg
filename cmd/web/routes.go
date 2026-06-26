155de5d47bc15996122ac0fec4c9665cd92ef0fd
Tobias Gleiter <gleiter.tobias@outlook.de>
2026-06-26 19:11:49 -0400

    chore: added csv export

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -32,6 +32,7 @@ func (app *application) routes() (http.Handler, error) {
 	mux.HandleFunc("POST /orgs/{org_id}/delete", app.html.Handle(app.postDeleteOrg))
 	mux.HandleFunc("GET /orgs/{org_id}/equipment", app.html.Handle(app.getEquipment))
 	mux.HandleFunc("GET /orgs/{org_id}/equipment/print", app.html.Handle(app.getEquipmentPrint))
+	mux.HandleFunc("GET /orgs/{org_id}/equipment/export", app.html.Handle(app.getEquipmentExport))
 	mux.HandleFunc("GET /orgs/{org_id}/equipment/import", app.html.Handle(app.getEquipmentImport))
 	mux.HandleFunc("GET /orgs/{org_id}/equipment/import/template", app.html.Handle(app.getEquipmentImportTemplate))
 	mux.HandleFunc("POST /orgs/{org_id}/equipment/import", app.html.Handle(app.postEquipmentImport))
