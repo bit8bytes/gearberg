aabbea6e2d19e9821752e0b7310d6d8dab001594
Tobias Gleiter <gleiter.tobias@outlook.de>
2026-08-12 16:44:23 -0400

    chore: run nix/fmt

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 let
   pkgs = import <nixpkgs> {};
-  rev = builtins.fetchGit { url = ./.; }.shortRev;
-in import ./nixos/web.nix {
-  buildpkgs = pkgs;
-  version = rev;
-}
+  rev = builtins.fetchGit {url = ./.;}.shortRev;
+in
+  import ./nixos/web.nix {
+    buildpkgs = pkgs;
+    version = rev;
+  }
