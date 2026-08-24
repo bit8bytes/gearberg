b25f143a8c547538875eae91b0c39b5a24c0f18d
Tobias Gleiter <gleiter.tobias@outlook.de>
2026-08-24 19:14:39 -0400

    style: added widgets for overdue and soon inspections #35

--- cmd/web/handlers:dashboard.go
+++ cmd/web/handlers:dashboard.go
@@ -7,7 +7,14 @@ import (
 	"github.com/bit8bytes/gearberg/internal/templates/pages"
 )
 
+type dashboardData struct {
+	OrgID string
+}
+
 func (app *application) getDashboard(w http.ResponseWriter, r *http.Request) *httperr.Error {
 	tmplData := app.html.TemplateData(r)
+	tmplData.Data = dashboardData{
+		OrgID: r.PathValue("org_id"),
+	}
 	return app.html.Render(w, r, http.StatusOK, pages.Dashboard, tmplData)
 }
