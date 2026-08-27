e8271a8eaea65f1b1e6e47877201d730a3399400
Tobias Gleiter <gleiter.tobias@outlook.de>
2026-08-27 18:56:09 -0400

    chore: redirect to equipment page if only one org exists

--- cmd/web/handlers:orgs.go
+++ cmd/web/handlers:orgs.go
@@ -60,6 +60,11 @@ func (app *application) getOrgPicker(w http.ResponseWriter, r *http.Request) *ht
 		return httperr.InternalServerError(err)
 	}
 
+	if len(allOrgs) == 1 {
+		http.Redirect(w, r, "/orgs/"+allOrgs[0].ID+"/equipment", http.StatusSeeOther)
+		return nil
+	}
+
 	data := app.html.TemplateData(r)
 	data.Data = orgsData{
 		Orgs: allOrgs,
